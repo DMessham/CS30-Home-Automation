9e6cc5dc0dda71de7d5ea582c494bf59381f2f44
DMessham <20892935+DMessham@users.noreply.github.com>
2021-11-22 12:01:30 -0600

    ui assets, power/battery ui work

---
+++ power.js
@@ -0,0 +1,163 @@
+// MAJOR PROJECT UI
+// Daniel Messham
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let bat0 = {
+  percent=100,
+  voltage=3.7,
+  voltageChange=-0.0,
+  charging=true,
+  exists=true,
+  timeToEmpty=999999999999,
+  timeToFull=0,
+  progressbar,
+}
+
+let acIn0 = {
+  voltage=120,
+  amperage,
+  connected=true,
+}
+
+let dcIn0 = {
+  voltage=12,
+  amperage,
+  connected,
+  voltRangebar,
+
+}
+
+let carBat0 = {
+  percent=100,
+  voltage=12.5,
+  maxVolt=15,
+  minVoltage=11.5,
+  voltageChange=-0.0,
+  charging=true,
+  exists=true,
+  voltRangebar,
+}
+let carFueltank0 = {
+  percent=100,
+  capacityLiter=68,
+  usageLiter=0,
+  temperature=10,
+  usageRangeLkm=
+  usageRangeMpg
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(20);
+
+}
+
+
+
+class Button {
+  constructor(x,y,buttonWidth, buttonHeight, text, accent, norm, hover, txtcolor) {
+    this.x = x;
+    this.y = y;
+    this.width = buttonWidth;
+    this.height = buttonHeight;
+    this.normColor = norm
+    this.accentColor = accent
+    this.hoverColor = hover
+    this.text = text
+    this.textColor = txtcolor
+  }
+  display(){
+    if(this.isPointInButton(mouseX,mouseY)){
+      if(mouseIsPressed){
+        fill(this.accentColor);
+      }
+      else{fill(this.hoverColor);}
+    }
+    else{fill(this.normColor);}
+    stroke(this.accentColor)
+    strokeWeight(3)
+    rect(this.x,this.y,this.width,this.height)
+    noStroke;
+    textSize(this.height/4)
+    fill(this.textColor)
+    text(this.text,this.x+50,this.y+50,this.width-50,this.height-50)
+  }
+  isPointInButton(x,y){
+    return( x>this.x && x<this.x+this.width && y>this.y && y<this.y+this.height)
+  }
+}
+
+class progress { //idk how well this will work yet
+  constructor(x,y,objWidth, objHeight, progress, fill, bg, text, txtcolor) {
+    this.x = x;
+    this.y = y;
+    this.width = objWidth;
+    this.height = objHeight;
+    this.bgColor = bg
+    this.edgeColor = 160
+    this.fillColor = fill
+    this.progress = progress
+    this.label = text
+    this.textColor = txtcolor
+
+  }
+  display(){
+    fill(this.bgColor);
+    stroke(this.edgeColor)
+    strokeWeight(3)
+    rect(this.x,this.y,this.width,this.height)
+    fill(this.fillColor)
+    rect(this.x,this.y,this.width*(0.01*progress),this.height)
+    noStroke;
+    textSize(this.height/4)
+    fill(this.textColor)
+    text(this.label,this.x+50,this.y+50,this.width-50,this.height-50)
+  }
+  update(progress){
+    this.progress = progress;
+    
+  }
+}
+
+class toggle { // still wip
+  constructor(x,y,buttonWidth, buttonHeight, text, accent, norm, hover, txtcolor) {
+    this.x = x;
+    this.y = y;
+    this.width = buttonWidth;
+    this.height = buttonHeight;
+    this.normColor = norm
+    this.accentColor = accent
+    this.hoverColor = hover
+    this.text = text
+    this.textColor = txtcolor
+    this.state = false;
+    this.onColor = 'green';
+    this.offColor = 'red'
+  }
+  display(){
+
+    if(this.isPointInButton(mouseX,mouseY)){
+      if(mouseIsPressed){
+        fill(this.accentColor);
+      }
+      else{fill(this.hoverColor);}
+    }
+    else{fill(this.normColor);}
+    stroke(this.accentColor)
+    strokeWeight(3)
+    rect(this.x,this.y,this.width,this.height)
+    noStroke;
+    textSize(this.height/4)
+    fill(this.textColor)
+    text(this.text,this.x+50,this.y+50,this.width-50,this.height-50)
+  }
+  isPointInButton(x,y){
+    return(x>this.x && x<this.x+this.width && y>this.y && y<this.y+this.height)
+  }
+}
