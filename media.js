e03879a0d569c1d1b534bc17dbf7905d8fd4eb47
DMessham <20892935+DMessham@users.noreply.github.com>
2021-11-29 12:12:03 -0600

    major work on media ui

--- media.js
+++ media.js
@@ -6,16 +6,18 @@
 // - describe what you did to take this project "above and beyond"
 
 let media0 = {
-  sourceName:"[NO INPUT]",
-  sourceID:"none",
-  sourceType:"test",
+  sourceName:"[SOURCE_NAME]",
+  sourceID:"SOURCE",
+  sourceType:"[SOURCE_TYPE]",
+  sourceIcon:"none",
   mediaType:"audio",
   mediaCodec:"none",
   mediaStateName:0,
-  mediaStateString:"Stopped",
+  mediaStateString:"[PLAYBACK_STATUS]",
   mediaTitle:"[TEST_SONG_TITLE]",
   mediaArtistName:"[TEST_SONG_ARTIST]",
-  mediaAlbumName:"[TEST_SONG_NAME]",
+  mediaAlbumName:"[TEST_ALBUM_NAME]",
+  mediaAlbumArt:"null",
   mediaPlayedSec:60,
   mediaIsLive:false,
   mediaLengthSec:200,
@@ -36,7 +38,8 @@ function windowResized(){
 function draw() {
   background(20);
   testdraw()
-  drawMediaStat(media0, 10,25,drawWidth)
+  drawMediaStat(media0, 7,25,drawWidth,media0.mediaAlbumArt, 115, media0.sourceIcon)
+  mediaControl(media0, 15, 150, drawWidth, drawWidth/6, 15)
 }
 
 function testdraw() {
@@ -45,20 +48,48 @@ function testdraw() {
   rect(0,0,width,4)
 }
 
-function drawMediaStat(media, x,y,wid){
+function drawMediaStat(media, x,y,wid, art, artWidth, sourceIcon){
   fill('white');
-  textSize(17)
-  text(media.sourceName + " - " + media.sourceType + ": " + media.mediaStateString + " (" + media.mediaPlayedSec + "/" + media.mediaLengthSec + " Seconds )", x, y);
-  text(media.mediaArtistName + " - " + media.mediaTitle, x, y+20);
+  rect(x,y-20,30,30)//source icon placeholder
+  textSize(17)//source info
+  text(media.sourceName + " - " + media.sourceType + ": " + media.mediaStateString, x+33, y);
+  textSize(27)//song title
+  text(media.mediaTitle, x+artWidth+7, y+33);
+  textSize(17)//artist and album
+  text(media.mediaArtistName + " - " + media.mediaAlbumName, x+artWidth+7, y+60);
   if(!media0.Live){
   strokeWeight(1);
-  stroke("white");
+  stroke("white");//progressbar bg
   fill('gray');
-  rect(x,y+33,wid-(x*2), 13);
-  fill('green');
+  rect(x+7+artWidth,y+80,wid-(x*2)-artWidth-3, 13);
+  fill('green');//progressbar fill
   noStroke();
-  rect(x+1,y+36,(wid-(x*2)-1)*(media.mediaProgress/100),7);
+  rect(x+9+artWidth,y+83,(wid-(x*2)-4-artWidth)*(media.mediaProgress/100),7);
+  fill(`red`)//album art placeholder
+  rect(x, y+10, artWidth, artWidth)
+  textSize(17)//playeed time
+  fill('green');
+  text("played " + media.mediaPlayedSec , x+artWidth+10, y+113);
+  fill('gray');//time remaining
+  text("left: " + media.mediaLengthSec, wid-80, y+113);
   }
+}
+
+function mediaControl(source, x, y, wid, size, space){
+  fill(`cyan`)//restart
+  rect(x, y+space, size, size)
+
+  fill(`orange`)//back
+  rect(x + (size+space), y+space, size, size)
+
+  fill(`purple`)//play
+  rect(x + 2*(size+space), y+space, size, size)
+
+  fill(`orange`)//next
+  rect(x + 3*(size+space), y+space, size, size)
+
+  fill(`blue`)//options
+  rect(x + 4*(size+space), y+space, size, size)
 }
 
 class Button {
